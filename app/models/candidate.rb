6ae4ecafa7f6dd200396c1ddb8d88787b6785266
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-02-23 17:47:35 -0500

    Pas tests for votes that they exist and can be created.

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,6 +2,8 @@ class Candidate < ActiveRecord::Base
   has_many :votes
   validates :name, presence: true
   validates :hometown, presence: true
+  validates :district, presence: true
+
 
 
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "votes exist" do
+    assert Vote
+  end
+
+  test "votes can be created" do
+    fear_itself = Candidate.create!(name: "Frank Underwood", hometown: "Gaffney, South Carolina", district: "South Carolina", party: "Democrat")
+    sheep = Voter.create!(name: "Average American", party: "Both")
+    Vote.create!(candidate_id: fear_itself.id, voter_id: sheep.id)
+    refute Vote.all.blank?
+  end
 end
