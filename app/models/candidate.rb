334e00637a7ff4919b3f5934d7c6f0fd872fc8d7
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-02-23 17:25:17 -0500

    Pass tests for candidates where candidates exist and can be created.

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,7 @@
 class Candidate < ActiveRecord::Base
   has_many :votes
+  validates :name, presence: true
+  validates :hometown, presence: true
+
 
 end
--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
+require 'minitest/pride'
 
 class CandidateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "candidates can be bought" do
+    assert Candidate
+  end
+
+  test "candidates can be created" do
+    bern = Candidate.create!(name: "Bernie Sanders", hometown: "Burlington, Vermont", district: "Vermont's at-large", party: "Democrat")
+    racist = Candidate.create(name: "Donald Trump", district: "Top 1%", party: "Republican")
+
+    assert Candidate.where(name: "Bernie Sanders"), bern
+    refute Candidate.where(name: "Donald Trump").exists?
+  end
 end
